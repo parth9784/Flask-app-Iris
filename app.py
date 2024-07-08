43e875ce1980194abba70c06b8364665e5839dab
parth9784 <pdsharma458@gmail.com>
2024-07-08 15:06:51 +0530

    completed

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template,url_for,request
+import joblib
+import numpy as np
+app=Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/predict",methods=["GET","POST"])
+def predict():
+    if request.method=="POST":
+        model=joblib.load("svm_iris.pkl")
+        sl=float(request.form["sl"])
+        sw=float(request.form["sw"])
+        pl=float(request.form["pl"])
+        pw=float(request.form["pw"])
+        arr=np.array([[sl,sw,pl,pw]])
+        pred=model.predict(arr)
+        return render_template("result.html",ans=pred)
+
+if(__name__)=="__main__":
+    app.run(debug=True)
